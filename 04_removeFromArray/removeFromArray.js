6fcca7f66a1465a37c63e69a4d90f126ff6535c1
khanhngguyen <ntkimkhanh97@gmail.com>
2023-02-02 23:34:36 +0200

    solved 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+/* 
+input: [array], [item to remove]
+e.g. [1, 2, 3, 4, 5], [1, 5] => [2, 3, 4]
+check every item in [array], if matches items in [item to remove] => remove from [array]
+OR check every item in [array], if NOT match items in [item to remove] => return
+=> run a function for each item in [array], check if not match
+ */
 
+const removeFromArray = function(arr, ...toRemove) {
+let result = [];
+arr.forEach((item) => {
+    if (toRemove.includes(item) === false) {
+        return result.push(item);
+    }
+});
+return result;
 };
 
 // Do not edit below this line
