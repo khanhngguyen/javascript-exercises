ff70437ea7899594ee6a00e5049a42b5ac703822
khanhngguyen <ntkimkhanh97@gmail.com>
2023-02-03 01:07:25 +0200

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+/* 
+no negative num
+no non-number: no string, no array
+ */
 
+const sumAll = function(a, b) {
+    //no negative number
+    if (a < 0 || b < 0) return 'ERROR';
+    //no non-number: no string, no array
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') return 'ERROR';
+    let min;
+    let max;
+    if (a > b) {
+        max = a;
+        min = b;
+    } else {
+        max = b;
+        min = a;
+    };
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
