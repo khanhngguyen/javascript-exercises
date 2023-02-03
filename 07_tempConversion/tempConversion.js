5ad37a81b563ddcdbb0050e15c4b6828e01f233e
khanhngguyen <ntkimkhanh97@gmail.com>
2023-02-03 02:50:01 +0200

    solved 06_leapYears & 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = (num - 32) * (5 / 9);
+  return parseFloat(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = num * (9 /5) + 32;
+  return parseFloat(num.toFixed(1));
 };
 
 // Do not edit below this line
