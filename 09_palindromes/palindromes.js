8157e7583d540d560a6fc4ab612f44a91b686727
khanhngguyen <ntkimkhanh97@gmail.com>
2023-02-04 22:36:36 +0200

    solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const specialChar = /[^A-Za-z0-9]/g;
+    //to remove special characters from given string, then make it lower case:
+    let strNoSpecialChar = str.toLowerCase().replace(specialChar, '');
+    //palindrome: if string === reversed string => palindrome;
+    //to reverse string, convert to array, then use reverse(), then convert back to string
+    let reversedStr = strNoSpecialChar.split('').reverse().join('');
+    //now compare
+    return (strNoSpecialChar === reversedStr) ? true : false;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
