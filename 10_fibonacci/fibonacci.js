6009bf1b65b67f60659ca4b63592e3d10b0e0506
khanhngguyen <ntkimkhanh97@gmail.com>
2023-02-04 23:51:17 +0200

    solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    //error if user puts negative number
+    if (n < 0) return 'OOPS';
+    if (n === 0) return 0;
+    //first number in the Fibonacci series is 1
+    if (n === 1) return 1;
+    //declare a & b, first 2 numbers in the Fibonacci
+    let a = 0;
+    let b = 1;
+    //loop (n - 2) because we already have a & b
+    for (let i = 0; i < n - 2; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    //end of loop, a & b now are the 2 numbers preceding n
+    return a + b;
 };
 
 // Do not edit below this line
